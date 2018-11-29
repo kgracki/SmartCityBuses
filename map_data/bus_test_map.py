97c154f8108c5357e97db57b2f1a291faa3c388b
Kacper G <kacpergracki@gmail.com>
2018-11-29 22:33:01 +0100

    Added computing distance between buses

---
+++ map_data/bus_test_map.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : bus_test_map.py
+# Author            : Kacper Gracki <kacpergracki@gmail.com>
+# Date              : 29.11.2018
+# Last Modified Date: 29.11.2018
+# Last Modified By  : Kacper Gracki <kacpergracki@gmail.com>
+
+
+map_points = [
+    (10, 10),
+    (10, 11),
+    (15, 11),
+    (15, 12),
+    (10, 12),
+    (10, 13),
+    (20, 13),
+    (20, 11),
+    (20, 10),
+    (10, 10)
+]
