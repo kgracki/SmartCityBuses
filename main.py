672e79008a967669f5e7773675f750f82a7ce017
Konrad Stalęga <konradstalega@gmail.com>
2018-12-09 15:40:49 +0100

    Adding buses to the line

--- main.py
+++ main.py
@@ -9,16 +9,22 @@
 from agents.director import Director
 from agents.bus import Bus
 from credentials import *
+from bus_lines.bus_line import BusLine
 
 import time
 
 
 if __name__ == "__main__":
+    bus_line_414 = BusLine(414, 18400)
     director = Director(DIRECTOR, DIRECTOR_PASSWD)
     bus1 = Bus(BUS1, BUS1_PASSWD)
+    bus1.add_line(bus_line_414)
     bus2 = Bus(BUS2, BUS2_PASSWD)
+    bus2.add_line(bus_line_414)
     bus3 = Bus(BUS3, BUS3_PASSWD)
+    bus3.add_line(bus_line_414)
     bus4 = Bus(BUS4, BUS4_PASSWD)
+    bus3.add_line(bus_line_414)
     director.start()
     time.sleep(5)
     bus1.start()
